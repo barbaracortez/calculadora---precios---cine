9fc7b3c98112c515d1caf6bc688b2dc4f5b68c11
barbaracortez <cortezbarbara.dev@gmail.com>
2025-06-17 17:21:14 -0300

    Agregar botón para calcular precio de entrada

--- main.js
+++ main.js
@@ -16,8 +16,9 @@ document.getElementById("formulario").addEventListener("submit", (e) => {
   } else if (edad <= 64) {
     precio = 1000;
   } else {
-    precio = 700;
+   precio = 700;
   }
 
   resultado.textContent = "Tu entrada cuesta: $ " + precio;
 });
+
